b7f18a909e1398745f9655f7f5bcf5bbab9f4903
Claudiu-Dan Balint <cbalint@deloittece.com>
2023-08-09 16:50:48 +0300

    spring_in_action: first commit

---
+++ taco-cloud/src/test/java/spring/in/action/fifth/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.in.action.fifth.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
